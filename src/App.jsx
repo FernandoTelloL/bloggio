08b445a3237120e4afcb3dcce0642adb3aad6f59
FernandoTelloL <fernando_tellol@hotmail.com>
2024-03-05 00:55:35 -0500

    agregado tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+
+  return (
+    <>
+     <h1 className="text-3xl font-bold underline p-4 border-2 m-2 rounded-md text-center uppercase bg-pink-200 text-blue-600">
+      Hello world!
+    </h1>
+    </>
+  )
+}
+
+export default App
