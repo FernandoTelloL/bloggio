ee835d8c4c45922f6b1bbd4a8c19d580b6bd2412
FernandoTelloL <fernando_tellol@hotmail.com>
2024-03-15 02:10:27 -0500

    agregado estilos botones navegacion tamaño desktop

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -10,7 +10,7 @@ export const Pagination = () => {
             <span className="hidden md:block px-2"> Anterior </span>
           </a>
         </li>
-        <div className="flex gap-2 items-center">
+        <div className="flex gap-2 md:gap-4 lg:gap-6 items-center">
           <li className="page-item bg-slate-900 rounded-full w-4 h-4 flex justify-center items-center p-3">
             <a className="page-link" href="#">
               1
