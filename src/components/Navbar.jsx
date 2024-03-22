3527a002d89945c74701a87182daddfe91209203
FernandoTelloL <fernando_tellol@hotmail.com>
2024-03-22 09:58:18 -0500

    agregado imagen de avatar y submenu de configuracion

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,12 +3,17 @@ import { useState } from "react";
 import burgerMenu from "../assets/icons/icon-hamburger.svg";
 import closeMenu from "../assets/icons/icon-menu-close.svg";
 
+import userPhoto from "../assets/images/user-photo.jpg";
+
 export const Navbar = () => {
   const [menuClicked, setMenuClicked] = useState(false);
+  const [photoClicked, setPhotoClicked] = useState(false);
 
   const handleClick = () => {
     setMenuClicked(() => !menuClicked);
   };
+
+  const handlePhotoClick = () => [setPhotoClicked(() => !photoClicked)];
 
   return (
     <>
@@ -17,18 +22,20 @@ export const Navbar = () => {
           menuClicked ? "!right-0" : ""
         }`}
       >
-        <div className="p-6 md:hidden" onClick={handleClick}>
-          <img className="w-12 h-12 ml-auto" src={closeMenu} alt="" />
+        <div className="flex justify-end">
+          <div className="p-6 md:hidden" onClick={handleClick}>
+            <img className="w-12 h-12" src={closeMenu} alt="" />
+          </div>
         </div>
 
-        <div className="flex flex-col mt-20 items-center h-full md:flex-row md:mt-0 md:h-auto md:justify-end">
+        <div className="flex flex-col mt-10 items-center h-full md:flex-row md:mt-0 md:h-auto md:justify-end">
           {/* TODO: Cambiar clases de color hover */}
           <li className="list-none mb-8 sm:mb-0 md:cursor-pointer border-b border-b-gray-500 w-[60%] text-center leading-loose md:leading-none md:text-start md:w-fit md:mr-2 md:border-none">
             <a
               className="text-2xl font-bold uppercase hover:text-SoftRed text-4 p-3 md:hover:text-red-800 md:hover:border-b md:hover:border-red-800 transition-all md:text-base"
               href="#"
             >
-              Home
+              Inicio
             </a>
           </li>
 
@@ -37,7 +44,7 @@ export const Navbar = () => {
               className="text-2xl font-bold uppercase hover:text-SoftRed text-4 p-3 md:hover:text-red-800 md:hover:border-b md:hover:border-red-800 transition-all md:text-base"
               href="#"
             >
-              Categories
+              Categorias
             </a>
           </li>
 
@@ -46,9 +53,38 @@ export const Navbar = () => {
               className="text-2xl font-bold uppercase hover:text-SoftRed text-4 p-3 md:hover:text-red-800 md:hover:border-b md:hover:border-red-800 transition-all md:text-base"
               href="#"
             >
-              About us
+              Nosotros
             </a>
           </li>
+
+          <div className=" object-cover relative flex justify-center">
+            <img
+              className="w-14 h-14 object-cover rounded-full object-top border-2 border-slate-950"
+              onClick={handlePhotoClick}
+              src={userPhoto}
+              alt=""
+            />
+
+            {/* inicio submenu foto perfil */}
+            <div
+              className={`bg-slate-950 text-slate-200 text-sm absolute top-16 py-2 px-4 rounded-lg ${
+                photoClicked ? "block" : "hidden"
+              }`}
+            >
+              {/* TODO: hacer bien el after para que se vea una flecha hacia arriba */}
+              <ul className="after:content[:''] after:h-2 after:bg-blue-400">
+                <li>
+                  <a className="" href="#">
+                    Mi perfil
+                  </a>
+                </li>
+                <li>
+                  <a href="#">Configuración</a>
+                </li>
+              </ul>
+            </div>
+            {/* fin submenu foto perfil */}
+          </div>
         </div>
       </ul>
 
