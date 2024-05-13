cddce5f4d718832a8e418f7d4d5721c96eb1d251
FernandoTelloL <fernando_tellol@hotmail.com>
2024-05-12 19:43:30 -0500

    last updates

--- src/sections/categoriesPage/SearchCategories.jsx
+++ src/sections/categoriesPage/SearchCategories.jsx
@@ -34,7 +34,7 @@ export const SearchCategories = () => {
             name='searchCategories'
             id='searchCategories'
             placeholder='Ingresa categoría o título'
-            className='p-2 w-full rounded-lg  text-sm ring-1 ring-slate-400'
+            className='p-2 w-full rounded-lg text-sm ring-1 ring-slate-400'
           />
           <button className='bg-amber-400 rounded-lg ml-2 py-2 px-4 font-bold' onClick={handleOpenFiltersModal}>
             Filtros
