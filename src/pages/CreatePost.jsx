790aa5e6224fa603d05ec016dc49e0288281d40e
Ricardo Bueno <ricardo_23_91@hotmail.com>
2024-05-13 01:32:32 -0500

    modify url for test endpoint create

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -50,6 +50,7 @@ export const CreatePost = () => {
 
     // Datos que enviarás en el cuerpo de la petición POST
     const dataPrueba = {
+      postId: '',
       categoryId: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
       postContent: 'Piero',
       postDescription: 'chipiiii',
