1b3d5d9e74e6041ac7a5c3e6ad11135ce92dc28b
FernandoTelloL <fernando_tellol@hotmail.com>
2024-05-18 19:10:46 -0500

    agregado loading en login y crear usuario

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,12 +1,15 @@
+import { useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { Headers } from './../components'
 import { useUserStore } from '../store/userStore'
 import { useNavigate } from 'react-router-dom'
 import { ShowErrorAlert } from '../utils/ShowErrorAlert'
+import { MutatingDots } from 'react-loader-spinner'
 
 export const LoginPage = () => {
   const basepath = 'https://bloggio-blo-latest.onrender.com'
   const navigate = useNavigate()
+  const [loading, setLoading] = useState(false) // Estado para el spinner
 
   const {
     register,
@@ -18,6 +21,7 @@ export const LoginPage = () => {
 
   const onSubmit = async ({ username, password }) => {
     const dataFormated = { userNickname: username, userPassword: password }
+    setLoading(true) // Mostrar spinner
     try {
       const response = await fetch(`${basepath}/auth/signin`, {
         method: 'POST',
@@ -41,6 +45,8 @@ export const LoginPage = () => {
     } catch (error) {
       console.error('Error en la solicitud de login:', error)
       ShowErrorAlert('Ocurrió un error. Por favor, intenta de nuevo más tarde.')
+    } finally {
+      setLoading(false) // Ocultar spinner
     }
   }
 
@@ -48,10 +54,24 @@ export const LoginPage = () => {
     <>
       <Headers />
 
+      {loading && (
+        <div className='fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-30'>
+          <MutatingDots
+            visible
+            height='100'
+            width='100'
+            color='#4fa94d'
+            secondaryColor='#4fa94d'
+            radius='12.5'
+            ariaLabel='mutating-dots-loading'
+          />
+        </div>
+      )}
+
       <div className='flex flex-col justify-between mt-20'>
         <div className='flex flex-col justify-center items-center '>
           <h1 className='text-2xl font-extrabold mb-4 md:text-3xl md:mb-8'>
-            Login
+            Login de Usuario
           </h1>
           <form
             className='w-[90%] md:w-2/3 lg:w-1/3'
@@ -101,7 +121,7 @@ export const LoginPage = () => {
             <input
               type='submit'
               value='Enviar'
-              className='bg-slate-950 text-slate-300 px-5 py-2 rounded-lg block w-[100%] shadow-2xl md:mt-10 md:py-4'
+              className='bg-slate-950 text-slate-300 px-5 py-2 rounded-lg block w-[100%] shadow-2xl md:mt-10 md:py-4 cursor-pointer hover:scale-105 transition-all'
             />
           </form>
         </div>
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,12 +1,15 @@
+import { useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { Headers } from './../components'
 import { useUserStore } from '../store/userStore'
 import { useNavigate } from 'react-router-dom'
 import { ShowErrorAlert } from '../utils/ShowErrorAlert'
+import { MutatingDots } from 'react-loader-spinner'
 
 export const SignUp = () => {
   const basepath = 'https://bloggio-blo-latest.onrender.com'
   const navigate = useNavigate()
+  const [loading, setLoading] = useState(false) // Estado para el spinner
 
   const {
     register,
@@ -18,14 +21,13 @@ export const SignUp = () => {
 
   const onSubmit = async ({ username, email, password }) => {
     const dataFormated = {
-      roles: [
-        'T_ROLE_USER'
-      ],
+      roles: ['T_ROLE_USER'],
       userEmail: email,
       userNickname: username,
       userPassword: password
     }
 
+    setLoading(true) // Mostrar spinner
     try {
       const response = await fetch(`${basepath}/auth/signup`, {
         method: 'POST',
@@ -49,12 +51,28 @@ export const SignUp = () => {
     } catch (error) {
       console.error('Error en la solicitud de login:', error)
       ShowErrorAlert('Ocurrió un error. Por favor, intenta de nuevo más tarde.')
+    } finally {
+      setLoading(false) // Ocultar spinner
     }
   }
 
   return (
     <>
       <Headers />
+
+      {loading && (
+        <div className='fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-30'>
+          <MutatingDots
+            visible
+            height='100'
+            width='100'
+            color='#4fa94d'
+            secondaryColor='#4fa94d'
+            radius='12.5'
+            ariaLabel='mutating-dots-loading'
+          />
+        </div>
+      )}
 
       <div className='flex flex-col justify-between mt-20'>
         <div className='flex flex-col justify-center items-center '>
@@ -127,7 +145,7 @@ export const SignUp = () => {
             <input
               type='submit'
               value='Enviar'
-              className='bg-slate-950 text-slate-200 px-5 py-2 rounded-lg block w-[100%] shadow-2xl md:mt-10 md:py-4 cursor-pointer'
+              className='bg-slate-950 text-slate-200 px-5 py-2 rounded-lg block w-[100%] shadow-2xl md:mt-10 md:py-4 cursor-pointer hover:scale-105 transition-all'
             />
           </form>
         </div>
