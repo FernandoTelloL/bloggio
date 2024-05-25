21eda33488b65d878f9c9b83bfdffb5d6ce37ae5
FernandoTelloL <fernando_tellol@hotmail.com>
2024-05-25 03:52:39 -0500

    cambios en detail post

--- src/pages/DetailPost.jsx
+++ src/pages/DetailPost.jsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
 import {
   MainTitleDetailPostPage,
   MenuBottomDetailPost,
@@ -7,73 +9,68 @@ import mainImage from '../assets/images/img1.webp'
 import secondaryImage from '../assets/images/img4.jpeg'
 
 export const DetailPost = () => {
+  const { id } = useParams() // Extrae el ID de los parámetros de la URL
+  const [post, setPost] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      try {
+        const response = await fetch(`https://bloggio-api.onrender.com/post/${id}`)
+        const data = await response.json()
+        setPost(data)
+        setLoading(false)
+      } catch (error) {
+        console.error('Error fetching post:', error)
+        setLoading(false)
+      }
+    }
+
+    fetchPost()
+  }, [id])
+
+  if (loading) {
+    return <div>Loading...</div>
+  }
+
+  if (!post) {
+    return <div>Error loading post</div>
+  }
+
   return (
     <>
       <div className='mt-16'>
-        <MainTitleDetailPostPage />
+        <MainTitleDetailPostPage title={post.title} />
 
         <img
           className='mt-16 mb-6 rounded-xl md:h-[300px] lg:h-[400px] md:w-full md:object-cover md:object-top lg:object-center'
-          src={mainImage}
-          alt=''
+          src={post.mainImage || mainImage}
+          alt={post.title}
         />
 
         <section className='mb-5 flex flex-col md:flex-row gap-2 lg:gap-4'>
-          <article className='md:w-[70%]'>
-            <p className='mb-3 first-line:tracking-widest first-letter:text-7xl first-letter:font-bold first-letter:mr-3 first-letter:float-left'>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. In
-              reprehenderit qui illum debitis odio nam consequuntur. Quisquam
-              aspernatur commodi est molestiae nostrum excepturi sint eius?
-            </p>
-
-            <p className='mb-3'>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. In
-              reprehenderit qui illum debitis odio nam consequuntur. Quisquam
-              aspernatur commodi est molestiae nostrum excepturi sint eius?
-            </p>
-
-            <p className='mb-3'>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet
-              non esse, quidem iusto aliquid officia repellendus labore
-              inventore blanditiis illo?
-            </p>
+          {/* <article className='md:w-[70%]'>
+            {post.content.split('\n').map((paragraph, index) => (
+              <p key={index} className='mb-3 first-line:tracking-widest first-letter:text-7xl first-letter:font-bold first-letter:mr-3 first-letter:float-left'>
+                {paragraph}
+              </p>
+            ))}
 
             <img
-              className='rounded-xl mt-6 mb-6 w-[80%] mr-auto ml-auto md:mt-10 md:mb-10 md:w-[70%] '
-              src={secondaryImage}
-              alt=''
+              className='rounded-xl mt-6 mb-6 w-[80%] mr-auto ml-auto md:mt-10 md:mb-10 md:w-[70%]'
+              src={post.secondaryImage || secondaryImage}
+              alt={post.title}
             />
 
-            <p className='mb-3'>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. In
-              reprehenderit qui illum debitis odio nam consequuntur. Quisquam
-              aspernatur commodi est molestiae nostrum excepturi sint eius?
-            </p>
-
-            <p className='mb-3'>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. In
-              reprehenderit qui illum debitis odio nam consequuntur. Quisquam
-              aspernatur commodi est molestiae nostrum excepturi sint eius?
-            </p>
-
-            <p className='mb-3'>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet
-              non esse, quidem iusto aliquid officia repellendus labore
-              inventore blanditiis illo?
-            </p>
-
             <MenuBottomDetailPost />
-          </article>
-
+          </article> */}
         </section>
-        <hr className=' bg-slate-500' />
+        <hr className='bg-slate-500' />
         <section className='mt-5 mb-6'>
           <RelatedPostsDetailPostPage />
         </section>
       </div>
-
       <hr className='mb-6 bg-slate-500' />
-
     </>
   )
 }
