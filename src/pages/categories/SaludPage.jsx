ccb11b5bf78e8c6df0372531dff45530e58aa0ca
FernandoTelloL <fernando_tellol@hotmail.com>
2024-06-01 01:15:46 -0500

    refactorizacion saludPage

--- src/pages/categories/SaludPage.jsx
+++ src/pages/categories/SaludPage.jsx
@@ -29,41 +29,49 @@ export const SaludPage = () => {
       categoryName: 'Salud',
       date_end: '',
       date_start: '',
-      limit: 10,
+      limit: 9,
       offset: 1,
       postTitle: ''
     })
   }, [limit, page])
 
   return (
-    <div>
-      {/* create grid to cards with tailwind */}
-      <div className='grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
-        {categorySalud.map((post) => (
-          <div key={post.postId} className='col-span-1 md:col-span-2 lg:col-span-3 xl:col-span-4'>
-            <div className='card' key={post.postId}>
-              <div className='card-body'>
-                <h3 className='card-title'>{post.postTitle}</h3>
-                <p className='card-text'>{post.postContent}</p>
+    <>
+      {loading && (
+        <div className='fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-30'>
+          <MutatingDots
+            visible
+            height='100'
+            width='100'
+            color='#172A99'
+            secondaryColor='#69141B'
+            radius='12.5'
+            ariaLabel='mutating-dots-loading'
+          />
+        </div>
+      )}
+
+      <section>
+
+        <h2 className='text-3xl font-bold lg:mt-16 mb-12'>Listado de Posts de categoria SALUD</h2>
+        <div className='grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+          {categorySalud.map((post) => (
+            <div key={post.postId} className='col-span-1 md:col-span-2 lg:col-span-3 xl:col-span-4'>
+              <div className='card' key={post.postId}>
+                <div className='card-body'>
+                  <h3 className='card-title'>{post.postTitle}</h3>
+                  <p className='card-text'>{post.postContent}</p>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
 
-      {/* Pagination */}
-      {
-        categorySalud.length > 0 && (
-          <>
-            <Pagination
-              page={page}
-              setPage={setPage}
-              limit={limit}
-              setLimit={setLimit}
-            />
-          </>
-        )
-      }
-    </div>
+        <section className='mb-12 lg:mb-24'>
+          <Pagination />
+        </section>
+
+      </section>
+    </>
   )
 }
