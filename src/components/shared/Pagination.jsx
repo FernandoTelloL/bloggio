6037604fab9e07643667477842f08f7f409322d5
FernandoTelloL <fernando_tellol@hotmail.com>
2024-07-20 19:41:33 -0500

    primera version de paginacion funcionando

--- src/components/shared/Pagination.jsx
+++ src/components/shared/Pagination.jsx
@@ -1,55 +1,115 @@
+// import { RiArrowLeftLine, RiArrowRightLine } from 'react-icons/ri'
+
+// export const Pagination = () => {
+//   return (
+//     <>
+//       <ul className='pagination flex justify-between items-center text-sm text-slate-500'>
+//         <li className='page-item border border-slate-500 rounded-full p-2'>
+//           <a className='page-link md:flex items-center gap-2' href='#'>
+//             <RiArrowLeftLine />
+//             <span className='hidden md:block px-2'> Anterior </span>
+//           </a>
+//         </li>
+//         <div className='flex gap-2 md:gap-4 lg:gap-6 items-center'>
+//           <li className='page-item bg-slate-900 text-slate-200 rounded-full w-4 h-4 flex justify-center items-center p-3 '>
+//             <a className='page-link' href='#'>
+//               1
+//             </a>
+//           </li>
+//           <li className='page-item '>
+//             <a className='page-link' href='#'>
+//               2
+//             </a>
+//           </li>
+//           <li className='page-item'>
+//             <a className='page-link' href='#'>
+//               3
+//             </a>
+//           </li>
+//           <li>...</li>
+//           <li className='page-item'>
+//             <a className='page-link' href='#'>
+//               8
+//             </a>
+//           </li>
+//           <li className='page-item'>
+//             <a className='page-link' href='#'>
+//               9
+//             </a>
+//           </li>
+//           <li className='page-item'>
+//             <a className='page-link' href='#'>
+//               10
+//             </a>
+//           </li>
+//         </div>
+//         <li className='page-item border border-slate-500 rounded-full p-2'>
+//           <a className='page-link md:flex items-center gap-2' href='#'>
+//             <span className='hidden md:block px-2'> Siguiente </span>
+//             <RiArrowRightLine />
+//           </a>
+//         </li>
+//       </ul>
+//     </>
+//   )
+// }
+
 import { RiArrowLeftLine, RiArrowRightLine } from 'react-icons/ri'
 
-export const Pagination = () => {
+export const Pagination = ({ currentPage, totalPages, onPageChange }) => {
+  const handlePrevious = (event) => {
+    event.preventDefault() // Prevenir el comportamiento por defecto del enlace
+    if (currentPage > 1) onPageChange(currentPage - 1)
+  }
+
+  const handleNext = (event) => {
+    event.preventDefault() // Prevenir el comportamiento por defecto del enlace
+    if (currentPage < totalPages) onPageChange(currentPage + 1)
+  }
+
+  const handlePageClick = (page, event) => {
+    event.preventDefault() // Prevenir el comportamiento por defecto del enlace
+    onPageChange(page)
+  }
+
+  const renderPageNumbers = () => {
+    const pageNumbers = []
+    for (let i = 1; i <= totalPages; i++) {
+      pageNumbers.push(
+        <li
+          key={i}
+          className={`page-item rounded-full ${i === currentPage ? 'bg-slate-900 text-slate-200' : 'bg-slate-100 text-slate-500'}`}
+        >
+          <button
+            className='page-link'
+            onClick={(event) => handlePageClick(i, event)}
+            disabled={i === currentPage} // Deshabilitar el botón de la página actual
+          >
+            {i}
+          </button>
+        </li>
+      )
+    }
+    return pageNumbers
+  }
+
   return (
-    <>
-      <ul className='pagination flex justify-between items-center text-sm text-slate-500'>
-        <li className='page-item border border-slate-500 rounded-full p-2'>
-          <a className='page-link md:flex items-center gap-2' href='#'>
-            <RiArrowLeftLine />
-            <span className='hidden md:block px-2'> Anterior </span>
-          </a>
-        </li>
-        <div className='flex gap-2 md:gap-4 lg:gap-6 items-center'>
-          <li className='page-item bg-slate-900 text-slate-200 rounded-full w-4 h-4 flex justify-center items-center p-3 '>
-            <a className='page-link' href='#'>
-              1
-            </a>
-          </li>
-          <li className='page-item '>
-            <a className='page-link' href='#'>
-              2
-            </a>
-          </li>
-          <li className='page-item'>
-            <a className='page-link' href='#'>
-              3
-            </a>
-          </li>
-          <li>...</li>
-          <li className='page-item'>
-            <a className='page-link' href='#'>
-              8
-            </a>
-          </li>
-          <li className='page-item'>
-            <a className='page-link' href='#'>
-              9
-            </a>
-          </li>
-          <li className='page-item'>
-            <a className='page-link' href='#'>
-              10
-            </a>
-          </li>
-        </div>
-        <li className='page-item border border-slate-500 rounded-full p-2'>
-          <a className='page-link md:flex items-center gap-2' href='#'>
-            <span className='hidden md:block px-2'> Siguiente </span>
-            <RiArrowRightLine />
-          </a>
-        </li>
-      </ul>
-    </>
+    <ul className='pagination flex justify-between items-center text-sm'>
+      <li className='page-item border border-slate-500 rounded-full p-2'>
+        <a className='page-link md:flex items-center gap-2' href='#' onClick={handlePrevious}>
+          <RiArrowLeftLine />
+          <span className='hidden md:block px-2'> Anterior </span>
+        </a>
+      </li>
+      <div className='flex gap-2 md:gap-4 lg:gap-6 items-center'>
+        {renderPageNumbers()}
+      </div>
+      <li className='page-item border border-slate-500 rounded-full p-2'>
+        <a className='page-link md:flex items-center gap-2' href='#' onClick={handleNext}>
+          <span className='hidden md:block px-2'> Siguiente </span>
+          <RiArrowRightLine />
+        </a>
+      </li>
+    </ul>
   )
 }
--- src/sections/homePage/AllPosts.jsx
+++ src/sections/homePage/AllPosts.jsx
@@ -1,3 +1,76 @@
+// import { useEffect, useState } from 'react'
+// import { MutatingDots } from 'react-loader-spinner'
+// import { fetchAllPosts } from '../../api'
+// import { CardType1, Pagination } from '../../components'
+
+// export const AllPosts = () => {
+//   const [posts, setPosts] = useState([])
+//   const [page, setPage] = useState([])
+//   console.log(page)
+//   const [loader, setLoader] = useState(true)
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//         const responseData = await fetchAllPosts(9, 1)
+//         console.log(responseData)
+//         // Verificar si responseData es un arreglo
+//         if (responseData && Array.isArray(responseData)) {
+//           setPosts(responseData[0])
+//           setPage(responseData[1])
+//           setLoader(false)
+//         } else {
+//           console.error('Data fetched is not an array:', responseData)
+//           setPosts([]) // Si no es un arreglo, setPosts a un arreglo vacío
+//         }
+//       } catch (error) {
+//         console.error('There was a problem with the fetch operation:', error)
+//       }
+//     }
+
+//     fetchData()
+//   }, [])
+
+//   return (
+
+//     loader
+//       ? (
+//         <div className='fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-30'>
+//           <MutatingDots
+//             visible
+//             height='100'
+//             width='100'
+//             color='#172A99'
+//             secondaryColor='#69141B'
+//             radius='12.5'
+//             ariaLabel='mutating-dots-loading'
+//           />
+//         </div>
+//         )
+//       : (
+//         <section>
+//           <h2 className='text-3xl font-bold'>Todos los posts</h2>
+//           <div className='md:grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
+//             {Array.isArray(posts) && posts.map((post, index) => (
+//               <CardType1
+//                 key={post.postId}
+//                 img={post.postImage}
+//                 title={post.postTitle}
+//                 description={post.postDescription}
+//                 userNickName={post.userNickname}
+//                 postCreated={post.postCreated}
+//                 postId={post.postId}
+//               />
+//             ))}
+//           </div>
+//           <section>
+//             <Pagination />
+//           </section>
+//         </section>
+//         )
+//   )
+// }
+
 import { useEffect, useState } from 'react'
 import { MutatingDots } from 'react-loader-spinner'
 import { fetchAllPosts } from '../../api'
@@ -5,19 +78,21 @@ import { CardType1, Pagination } from '../../components'
 
 export const AllPosts = () => {
   const [posts, setPosts] = useState([])
-  const [page, setPage] = useState([])
-  console.log(page)
+  const [page, setPage] = useState({ currentPage: 1, totalPages: 1 })
   const [loader, setLoader] = useState(true)
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const responseData = await fetchAllPosts(9, 1)
+        const responseData = await fetchAllPosts(6, page.currentPage)
         console.log(responseData)
         // Verificar si responseData es un arreglo
         if (responseData && Array.isArray(responseData)) {
           setPosts(responseData[0])
-          setPage(responseData[1])
+          setPage({
+            currentPage: responseData.page, // Página actual
+            totalPages: Math.ceil(responseData[1] / responseData[2]) // Total de páginas
+          })
           setLoader(false)
         } else {
           console.error('Data fetched is not an array:', responseData)
@@ -29,10 +104,13 @@ export const AllPosts = () => {
     }
 
     fetchData()
-  }, [])
+  }, [page.currentPage])
+
+  const handlePageChange = (newPage) => {
+    setPage(prevPage => ({ ...prevPage, currentPage: newPage }))
+  }
 
   return (
-
     loader
       ? (
         <div className='fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-30'>
@@ -49,9 +127,9 @@ export const AllPosts = () => {
         )
       : (
         <section>
-          <h2 className='text-3xl font-bold'>Todos los posts</h2>
+          <h2 className='text-3xl font-bold mb-6'>Todos los posts</h2>
           <div className='md:grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
-            {Array.isArray(posts) && posts.map((post, index) => (
+            {Array.isArray(posts) && posts.map(post => (
               <CardType1
                 key={post.postId}
                 img={post.postImage}
@@ -63,8 +141,12 @@ export const AllPosts = () => {
               />
             ))}
           </div>
-          <section>
-            <Pagination />
+          <section className='mt-6'>
+            <Pagination
+              currentPage={page.currentPage}
+              totalPages={page.totalPages}
+              onPageChange={handlePageChange}
+            />
           </section>
         </section>
         )
